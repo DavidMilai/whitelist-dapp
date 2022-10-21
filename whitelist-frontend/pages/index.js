7126972a7594e28a67192edee6c0418c9d721f4e
David Milai <davidmilai67@gmail.com>
2022-10-21 23:27:59 +0300

    UI

--- whitelist-frontend/pages/index.js
+++ whitelist-frontend/pages/index.js
@@ -1,10 +1,21 @@
 import Head from "next/head";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   const [numOfWhitelisted, setNumOfWhitelisted] = useState(0);
+  const web3ModalRef = useRef();
+
+  useEffect(()=>{
+    if(!walletConnected){
+      web3ModalRef.current = new web3ModalRef({
+        network: "goerli",
+        providerOptions:{},
+        disabledInjectedProvider: false,  
+      })
+    }
+  })
 
   return (
     <div>
@@ -21,9 +32,7 @@ export default function Home() {
           <img className={styles.image} src="./crypto-devs.svg" />
         </div>
       </div>
-      <footer className={styles.footer}>
-        Powered by Crypto Inc.
-      </footer>
+      <footer className={styles.footer}>Powered by Crypto Inc.</footer>
     </div>
   );
 }
